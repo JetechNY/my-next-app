0d49fb5028579c533e553df6f13f10bb31c659d1
JetechNY <sunjetliu@gmail.com>
2021-04-08 16:15:11 -0400

    Header added with some broken links

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+
+const about = () => {
+  return(
+    <div>
+<Head>
+  <title> About Page </title>
+</Head>
+      <hi> About</hi>
+    </div>
+  )
+}
+
+export default about
