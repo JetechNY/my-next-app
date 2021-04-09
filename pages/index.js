ac2c49c273cfb815a9606ea658cb4d878f7931e6
JetechNY <sunjetliu@gmail.com>
2021-04-08 23:15:25 -0400

    articles

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Layout.module.css";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
@@ -16,15 +17,30 @@ export default function Home() {
         </Link>
       </h3>
 
+      <ArticleList articles={articles}/>
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by :<span> Jet</span>ech
+          Powered by: Jetech
         </a>
       </footer>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
