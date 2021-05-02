02ada14872723bb1420ebf2d219745e2e6f34aff
JetechNY <sunjetliu@gmail.com>
2021-05-02 13:28:25 -0400

    edit

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ articles }) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by: Jetech Co.
+          Powered by: Jetech Inc.
         </a>
       </footer>
     </div>
